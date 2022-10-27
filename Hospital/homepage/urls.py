be4beee53f16f3bfb952890efb94e05ebebfd2a8
fidhamuhammad <insaahfathimz111@gmail.com>
2022-10-27 20:51:09 +0530

    updated

--- Hospital/homepage/urls.py
+++ Hospital/homepage/urls.py
@@ -3,7 +3,11 @@ from .import views
 
 urlpatterns=[
 
-    path('index',views.homepage_index)
-
+    path('index',views.homepage_index),
+    path('appoinment',views.homepage_appoinment),
+    path('about',views.homepage_about),
+    path('blogside',views.homepage_blogside),
+    path('blogsingle',views.homepage_blogsingle),
+    path('confirmation',views.homepage_confirmation)
 
 ]
--- Hospital/homepage/views.py
+++ Hospital/homepage/views.py
@@ -5,4 +5,29 @@ from django.shortcuts import render
 def homepage_index(request):
     return render(request,'homepage_template/index.html')
 
+def homepage_appoinment(request):
+    return render(request,'homepage_template/appoinment.html')
+
+def homepage_about(request):
+    return render(request,'homepage_template/about.html')
+
+def homepage_blogside(request):
+    return render(request,'homepage_template/blog-sidebar.html')  
+
+def homepage_blogsingle(request):
+    return render(request,'homepage_template/blog-single.html')
+
+def homepage_confirmation(request):
+    return render(request,'homepage_template/confirmation.html')
+
     
+
+
+
+
+
+
+
+
+
+
